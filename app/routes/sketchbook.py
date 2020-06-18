74a4945fbe893646fe15f6ed26bc514b3ba7da06
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-06-18 11:23:30 -0700

    Send follows with main page route request

--- app/routes/sketchbook.py
+++ app/routes/sketchbook.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from app.models import User, Sketchbook, db
+from app.models import Follow, User, Sketchbook, db
+from ..util import token_required
 
 bp = Blueprint("sketchbook", __name__, "")
 
@@ -8,11 +9,36 @@ bp = Blueprint("sketchbook", __name__, "")
 def getBasicSketchbooks():
     sketchbooks = Sketchbook.query.order_by(Sketchbook.timestamp).all()
     sketchbooks.reverse()
-    returnDict = dict()
+    sketchbookDict = dict()
     i = 1
     for book in sketchbooks:
-        returnDict[i] = {"owner_id": book.owner_id,
-                         "title": book.title,
-                         "timestamp": str(book.timestamp)}
+        sketchbookDict[i] = {"owner_id": book.owner_id,
+                             "sketchbook_id": book.id,
+                             "title": book.title,
+                             "timestamp": str(book.timestamp)}
         i += 1
+    print(sketchbookDict)
+    follows = Follow.query.all()
+    print(follows)
+    followList = []
+    for follow in follows:
+        followSublist = [follow.follower_id, follow.sketchbook_id]
+        followList.append(followSublist)
+    returnDict = dict()
+    returnDict['sketchbooks'] = sketchbookDict
+    returnDict['follows'] = followList
+    print(returnDict)
+    return returnDict
+
+
+@bp.route("/sketchbooks/<int:sk_id>/follow", methods=["POST"])
+@token_required
+def addFollow(current_user, sk_id):
+    newFollow = Follow(
+        sketchbook_id=sk_id,
+        follower_id=current_user.id
+    )
+    db.session.add(newFollow)
+    db.session.commit()
+    returnDict = {newFollow.sketchbook_id: True}
     return returnDict
